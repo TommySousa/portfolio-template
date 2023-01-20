253ea926befb7732446ca95be98f32158c668b9f
Tomás Sousa <118855300+TommySousa@users.noreply.github.com>
2023-01-20 16:56:35 +0000

    Added and styled the projects section

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const aboutInfo = document.querySelectorAll('.info');
 const infoContent = document.querySelectorAll('.info-content');
+const infoContentSkills = document.getElementById('skills');
 
-const show = (infoName) => {
+const addHighlight = () => {
+  aboutInfo.forEach((info) => {
+    info.addEventListener('click', (e) => {
+      removeActiveClass();
+      e.currentTarget.classList.add('is-active-info');
+      let contentTarget = e.currentTarget.attributes.pseudoid.value;
+      contentTarget = document.getElementById(contentTarget);
+      contentTarget.classList.add('is-active-content');
+    });
+  });
+};
+
+//this function removes the is active class from every class item
+const removeActiveClass = () => {
   aboutInfo.forEach((info) => {
     info.classList.remove('is-active-info');
   });
   infoContent.forEach((content) => {
     content.classList.remove('is-active-content');
   });
+  infoContentSkills.classList.remove('is-active-content');
 };
+
+addHighlight();
